ef6d054451b6db0641afaad2992590944bc7fd8f
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-01-06 18:18:53 -0800

    All is done

--- WordPlay/ResultsViewController.m
+++ WordPlay/ResultsViewController.m
@@ -18,12 +18,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSString * story = [NSString stringWithFormat:@"One day, %@ was walking into Mobile Makers when he noticed how %@ his students were. They %@ very %@", self.name, self.adjective, self.verb, self.adverb];
-    self.resultsTextView.text = story;
+    
+
+    //trying to bold
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:story];
+    //NSRange nameRange = NSMakeRange(9, 5); // 4 characters, starting at index 22
+    NSRange nameRange = [story rangeOfString:self.name];
+    NSString * replacedStory = [story stringByReplacingCharactersInRange:nameRange withString:[self stringReplacer:nameRange]];
+    NSRange adjectiveRange = [replacedStory rangeOfString:self.adjective];
+    replacedStory = [replacedStory stringByReplacingCharactersInRange:adjectiveRange withString:[self stringReplacer:adjectiveRange]];
+
+    NSRange verbRange = [replacedStory rangeOfString:self.verb];
+    replacedStory = [replacedStory stringByReplacingCharactersInRange:verbRange withString:[self stringReplacer:verbRange]];
+
+    NSRange adverbRange = [replacedStory rangeOfString:self.adverb];
+    replacedStory = [replacedStory stringByReplacingCharactersInRange:adverbRange withString:[self stringReplacer:adverbRange]];
+
+
+    [string beginEditing];
+
+    [string addAttribute:NSFontAttributeName
+                   value:[UIFont fontWithName:@"Helvetica-Bold" size:16.0]
+                   range:nameRange];
+    [string addAttribute:NSFontAttributeName
+                   value:[UIFont fontWithName:@"Helvetica-Bold" size:16.0]
+                   range:adjectiveRange];
+    [string addAttribute:NSFontAttributeName
+                   value:[UIFont fontWithName:@"Helvetica-Bold" size:16.0]
+                   range:verbRange];
+    [string addAttribute:NSFontAttributeName
+                   value:[UIFont fontWithName:@"Helvetica-Bold" size:16.0]
+                   range:adverbRange];
+    
+    
+    [string endEditing];
+
+    //setting textview
+     self.resultsTextView.attributedText = string;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSString* )stringReplacer:(NSRange)range {
+    NSString *returnString = [@"" stringByPaddingToLength:range.length withString:@"-" startingAtIndex:0];
+    return returnString;
 }
 
 /*
